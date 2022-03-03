b85093d720e77c3a98f8168eac51f8eb48f04637
Async <93988487+AsyncRef@users.noreply.github.com>
2022-03-03 10:45:53 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+int Brain_precision = 0;
+
+float Var;
+
+// hat block
+
+int whenStarted1() { 
+  var = 0.0;
+  return 0;
+}
+// when pressed
+void onevent_Bumper10_pressed_0() {
+  if (Var == 0.0) {
+    Var = 1.0;
+    TouchLED2.serColor(green);
+    Drivetrain.driveFor(forward, 200.0, mm);
+}
+else {
+  Var = 0.0;
+  TouchLED2.setColor(red);
+  Drivetrain.stop();
+}
+
+int main() {
+// register event handlers
+  Bumper10.pressed(onevent_Bumper10_pressed_0);
+
+  wait(15, msec);
+  whenStarted1();
+}
